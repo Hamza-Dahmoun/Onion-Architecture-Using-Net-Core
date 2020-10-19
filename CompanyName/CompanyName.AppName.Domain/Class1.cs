c2742399c1ea2f1c62824dff6af13a488d72641c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-19 15:31:26 +0100

    add Indemnisation.cs entity in Entities new folder, & install componentModel.Annotations nugt pkg

--- CompanyName/CompanyName.AppName.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CompanyName.AppName.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ CompanyName/CompanyName.AppName.Domain/Entities/Indenisation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CompanyName.AppName.Domain.Entities
+{
+    public class Indenisation
+    {
+        [Required(ErrorMessage="Id is required")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Number is equired")]
+        public string Number { get; set; }
+    }
+}
