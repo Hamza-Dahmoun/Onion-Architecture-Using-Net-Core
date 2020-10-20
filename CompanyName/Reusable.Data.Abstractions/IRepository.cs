1ef290e2522c01890e61deb0e6a6cc69f6f411c6
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-20 16:26:06 +0100

    add IRepository.cs

---
+++ CompanyName/Reusable.Data.Abstractions/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Reusable.Data.Abstractions
+{
+    public interface IRepository<T, TKey> where T: class, IEntity<TKey>
+    {
+        T GetById(TKey Id);
+        T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        List<T> GetAll(params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        PagedResult<T> GetAllPaged(string orderBy, int startRowIndex = 0, int maxRows = 10, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        List<T> GetAllFiltered(Expression<Func<T, bool>> prediacte, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        PagedResult<T> GetAllFilteredPaged(Expression<Func<T, bool>> predicate, string orderBy, int startRowIndex = 0, int maxRows = 10, params Expression<Func<T, object>>[] navigationPropertiesToLooad);
+
+        int Count();
+        int Count(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
