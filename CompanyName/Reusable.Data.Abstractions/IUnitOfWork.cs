a9e1c8236f8e3e618fe80dd190d8bc6e9f5458be
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-20 16:29:28 +0100

    add interface IUnitOfWork.cs

---
+++ CompanyName/Reusable.Data.Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reusable.Data.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<T, TKey> GetRepository<T, TKey>() where T : class, IEntity<TKey>;
+        int SaveChanges();
+    }
+}
