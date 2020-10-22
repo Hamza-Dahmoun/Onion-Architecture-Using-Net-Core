fc2d1120b75238c5991215cf5e933d07ce9ef30e
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-22 14:22:27 +0100

    add UnitOfWork.cs which implements IUnitOfWork interface

---
+++ CompanyName/Reusable.Data.Core/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reusable.Data.Abstractions;
+using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reusable.Data.Core
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public IRepository<T, TKey> GetRepository<T, TKey>() where T: class, IEntity<TKey>
+        {
+            if (!_repositories.ContainsKey(typeof(T)))
+                _repositories[typeof(T)] = new GenericRepository<T, TKey>(_dbContext);
+
+            return _repositories[typeof(T)] as IRepository<T, TKey>;
+        }
+    }
+}
