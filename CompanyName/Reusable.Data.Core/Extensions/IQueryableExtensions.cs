afcb9b16ee00482f55504dbfead02195beaf3260
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-10-22 11:10:55 +0100

    added class IqueryableExtensions.cs & wrote method inside IncludeAll() which will b used to load navigationProperties inside repositories when queries dbSets

---
+++ CompanyName/Reusable.Data.Core/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reusable.Data.Core
+{
+    //THIS CLASS AND METHOD IS GOING TO BE USED TO LOAD NAVIGATION PROPERTIES INSIDE REPOSITORIES WHEN QUERYING DBSETS
+
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> IncludeAll<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] navigationPropertiesToLoad) where T:class
+        {
+            foreach (var item in navigationPropertiesToLoad)
+            {
+                query = query.Include(item);
+            }
+            return query;
+        }
+    }
+}
