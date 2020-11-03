530d0a50af87b447887ffa80b14dba8f0c6e7a4a
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-03 14:59:19 +0100

    add AppDbContext.cs & override OnConfiguring() method

---
+++ CompanyName/CompanyName.AppName.Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace CompanyName.AppName.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext():base()
+        {
+
+        }
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //lets pass the connection string hard coded
+            optionsBuilder.UseSqlServer("Server=.;Database=AppDb;Integrated Security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- CompanyName/CompanyName.AppName.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CompanyName.AppName.Data
-{
-    public class Class1
-    {
-    }
-}
