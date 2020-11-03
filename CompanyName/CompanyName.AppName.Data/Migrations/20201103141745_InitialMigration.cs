2508fea5233656111170e5cae62ec1d1ac0c0d0c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-03 15:23:20 +0100

    added an initial migration & applied it to create the db

---
+++ CompanyName/CompanyName.AppName.Data/Migrations/20201103141745_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyName.AppName.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
