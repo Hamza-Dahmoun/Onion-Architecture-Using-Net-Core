e1bcc201c6bdd3a5d57f5139db5474a46a347f78
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-03 16:26:21 +0100

    add IBusinessService.cs interface & its methods signatures inside Reusable.Business.Core/BusinessResult

---
+++ CompanyName/Reusable.Business.Core/IBusinessService.cs
@@ -0,0 +1,22 @@
+﻿using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Reusable.Business.Core
+{
+    public interface IBusinessService<T, TKey> where T: class, IEntity<TKey>
+    {
+        T GetById(TKey id, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        List<T> GetAll(params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        PagedResult<T> GetAllPaged(string orderBy, int startRowIndex = 1, int maxRows = 10, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        List<T> GetAllFiltered(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+        PagedResult<T> GetAllFilteredPaged(Expression<Func<T, bool>> predicate, string orderBy, int startRowIndex = 1, int maxRows = 10, params Expression<Func<T, object>>[] navigationPropertiesToLoad);
+
+        Reusable.Business.Core.BusinessResult.BusinessResult Add(T entity);
+        Reusable.Business.Core.BusinessResult.BusinessResult Update(T entity);
+        Reusable.Business.Core.BusinessResult.BusinessResult Delete(T entity);
+    }
+}
