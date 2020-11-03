ffb47e74f637fe97beef21ceb762bbd3f2114e9c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-03 16:20:24 +0100

    add BusinessResult.cs class to Reusable.Business.Core/BusinessResult

---
+++ CompanyName/Reusable.Business.Core/BusinessResult/BusinessResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reusable.Business.Core.BusinessResult
+{
+    /// <summary>
+    /// This class represents the result of a busines operation execution.
+    /// The Succeeded property value indicate the succes or not of the busines operation.
+    /// The Messages property contains the list of messages returned in case of success, warning or error.
+    /// </summary>
+    
+    public class BusinessResult
+    {
+        public BusinessResult()
+        {
+            Messages = new HashSet<MessageResult>();//!!!
+        }
+
+        public ICollection<MessageResult> Messages { get; }
+        public bool Succeeded { get; protected set; }
+        public static BusinessResult Success 
+        {
+            get
+            {
+                var result = new BusinessResult { Succeeded = true };
+                result.Messages.Add(new MessageResult { Message = "Operation done successfully", MessageType = MessageType.Success });
+                return result;
+            }
+        }
+    }
+}
