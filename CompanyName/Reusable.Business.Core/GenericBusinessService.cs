503100b13499244ea6ed2a2c856f028cc7648bde
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-04 09:19:08 +0100

    add GenericBusinessService.cs class & its ctor to Reusable.Business.Core

---
+++ CompanyName/Reusable.Business.Core/GenericBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using Reusable.Data.Abstractions;
+using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reusable.Business.Core
+{
+    public class GenericBusinessService<T, TKey> where T: class, IEntity<TKey>
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IRepository<T, TKey> _repository;
+        public GenericBusinessService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = _unitOfWork.GetRepository<T, TKey>();
+        }
+    }
+}
