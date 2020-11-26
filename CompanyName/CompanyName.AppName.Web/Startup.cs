4d0d6e00dea75476953a399d668df06de3419515
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-26 16:22:58 +0100

    register dbContext services inside startup file

--- CompanyName/CompanyName.AppName.Web/Startup.cs
+++ CompanyName/CompanyName.AppName.Web/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyName.AppName.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace CompanyName.AppName.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //registering db context services, note that the connection string is the one inside appsettings.json file
+            services.AddScoped<DbContext, AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionStrings")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
