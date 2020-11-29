3828f94390f22a7a51841fff2b47866973428a38
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-29 16:06:49 +0100

    add ReferentielBusinessService.cs file

---
+++ CompanyName/CompanyName.AppName.Business/ReferentielBusinessService.cs
@@ -0,0 +1,20 @@
+﻿using CompanyName.AppName.Domain.Entities;
+using Reusable.Business.Core;
+using Reusable.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyName.AppName.Business
+{
+    public class ReferentielBusinessService : GenericBusinessService<Referentiel, Guid>
+    {
+        public ReferentielBusinessService(IUnitOfWork unitOfWork): base(unitOfWork)
+        {
+
+        }
+
+
+        
+    }
+}
