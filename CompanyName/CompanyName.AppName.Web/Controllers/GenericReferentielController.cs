6673ccc4534d85abca7869af29ec8ebc8cfc9d60
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-29 16:32:16 +0100

    added GenericReferentielController.cs & injected its business service in its ctor

---
+++ CompanyName/CompanyName.AppName.Web/Controllers/GenericReferentielController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyName.AppName.Business;
+using CompanyName.AppName.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Reusable.Domain.Core;
+
+namespace CompanyName.AppName.Web.Controllers
+{
+    public class GenericReferentielController<T, TKey> : Controller where T: Referentiel, IEntity<TKey>
+    {
+        protected readonly ReferentielBusinessService<T, TKey> _referentielBusinessService;
+
+        public GenericReferentielController(ReferentielBusinessService<T, TKey> referentielBusinessService)
+        {
+            _referentielBusinessService = referentielBusinessService;
+        }
+    }
+}
