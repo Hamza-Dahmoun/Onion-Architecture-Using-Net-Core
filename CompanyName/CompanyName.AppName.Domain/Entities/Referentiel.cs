9586d7ec52ecd690af897aa3cc33400a6ce43883
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-29 15:55:12 +0100

    add a parent class Referentiel.cs

---
+++ CompanyName/CompanyName.AppName.Domain/Entities/Referentiel.cs
@@ -0,0 +1,26 @@
+﻿using Reusable.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CompanyName.AppName.Domain.Entities
+{
+    public abstract partial class Referentiel : IEntity
+    {
+        //this cass is a parent class of all coming classes that have {Id, Code, Description} a common properties
+        //e.g: Wilaya, Municipalty...etc
+
+        [Display(Name = "Id")]
+        [Required(ErrorMessage = "RequiredMessage")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Code")]
+        [Required(ErrorMessage = "RequiredMessage"), StringLength(10, ErrorMessage = "StringLengthMessage")]
+        public string Code { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "RequiredMessage"), StringLength(256, ErrorMessage = "StringLengthMessage")]
+        public string Designation { get; set; }
+    }
+}
