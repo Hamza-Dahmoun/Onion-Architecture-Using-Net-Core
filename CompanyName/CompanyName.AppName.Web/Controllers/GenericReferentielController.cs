5291823a7dec7ea98642e7251e0840db9fb0523f
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-11-30 16:21:12 +0100

    add Index() action to genericReferentielController

--- CompanyName/CompanyName.AppName.Web/Controllers/GenericReferentielController.cs
+++ CompanyName/CompanyName.AppName.Web/Controllers/GenericReferentielController.cs
@@ -11,11 +11,41 @@ namespace CompanyName.AppName.Web.Controllers
 {
     public class GenericReferentielController<T, TKey> : Controller where T: Referentiel, IEntity<TKey>
     {
+        //Mentor:
+        //this controller is used as base controller for any Referentiel entity to take advantage of this
+        //default implementation and override any action when needed in derived controllers.
+
         protected readonly ReferentielBusinessService<T, TKey> _referentielBusinessService;
 
         public GenericReferentielController(ReferentielBusinessService<T, TKey> referentielBusinessService)
         {
             _referentielBusinessService = referentielBusinessService;
+        }
+
+        public virtual ActionResult Index(string search, int page =1, int pageSize=10)
+        {
+            PagedResult<T> model;
+
+            if (page < 1)
+                page = 1;
+
+            if (pageSize > 50)
+                pageSize = 50;
+
+            if (string.IsNullOrEmpty(search))
+            {
+                model = _referentielBusinessService.GetAllFilteredPaged(x => x.Code.StartsWith(search) || x.Description.Contains(search),
+                    "Code", (page-1)*pageSize, pageSize);
+            }
+            else
+            {
+                model = _referentielBusinessService.GetAllPaged("Code", (page-1)*pageSize, pageSize);
+            }
+
+            //the below data 'ViewBag.search' is going to be re-written inside the search input in the UI 
+            ViewBag.search = search;
+
+            return View(model);
         }
     }
 }
