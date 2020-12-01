0b3dcdefc3bd21f5f0a9017384b56565f1ce2e2c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-01 10:33:26 +0100

    add Commune & Wilaya entities

---
+++ CompanyName/CompanyName.AppName.Domain/Entities/Commune.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CompanyName.AppName.Domain.Entities
+{
+    public class Commune:Referentiel
+    {
+        [Display(Name="WilayaId")]
+        [Required(ErrorMessage ="RequiredMessage")]
+        public Guid WilayaId { get; set; }
+        public virtual Wilaya Wilaya { get; set; }
+    }
+}
---
+++ CompanyName/CompanyName.AppName.Domain/Entities/Wilaya.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyName.AppName.Domain.Entities
+{
+    public class Wilaya:Referentiel
+    {
+        public Wilaya()
+        {
+            this.Communes = new HashSet<Commune>();
+        }
+
+        public virtual ICollection<Commune> Communes { get; set; }
+    }
+}
