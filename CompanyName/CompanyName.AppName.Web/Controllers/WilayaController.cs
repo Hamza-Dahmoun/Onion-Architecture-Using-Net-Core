2c281b01f6fdb1613ec144f017027473bfc70751
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-01 11:48:27 +0100

    add WilayaController that inherits from ReferentielController

---
+++ CompanyName/CompanyName.AppName.Web/Controllers/WilayaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyName.AppName.Business;
+using CompanyName.AppName.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.AppName.Web.Controllers
+{
+    public class WilayaController : GenericReferentielController<Wilaya, Guid>
+    {
+        public WilayaController(ReferentielBusinessService<Wilaya, Guid> referentielBusinessService): base(referentielBusinessService)
+        {
+
+        }
+    }
+}
