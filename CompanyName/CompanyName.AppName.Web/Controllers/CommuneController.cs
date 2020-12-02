5ac892277d40af9315d951cf1b073b3996914eb0
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-02 14:53:22 +0100

    add CommuneController.cs file

---
+++ CompanyName/CompanyName.AppName.Web/Controllers/CommuneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyName.AppName.Business;
+using CompanyName.AppName.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.AppName.Web.Controllers
+{
+    public class CommuneController : GenericReferentielController<Commune, Guid>
+    {
+        private readonly ReferentielBusinessService<Wilaya, Guid> _wilayaBusinessService;
+        public CommuneController(ReferentielBusinessService<Commune, Guid> communeBusinessService, ReferentielBusinessService<Wilaya, Guid> wilayaBusinessService) :base(communeBusinessService)
+        {
+            _wilayaBusinessService = wilayaBusinessService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
