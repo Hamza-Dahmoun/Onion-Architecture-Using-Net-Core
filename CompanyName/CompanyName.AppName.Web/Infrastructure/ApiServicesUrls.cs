9ea141cb65b791d212850226796f6413173bdb0d
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-03 11:59:27 +0100

    add classes: ApiServicesUrls & AppSettings to CompanyName.AppName.Web/Infrastructure

---
+++ CompanyName/CompanyName.AppName.Web/Infrastructure/ApiServicesUrls.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName.Web.Infrastructure
+{
+    public class ApiServicesUrls
+    {
+        //Api Urls are generated using this helper class.
+        //you will inject this class in constructors or views to generate api urls based on model
+        //classes.In this manner you will never hard code urls in views or controllers.
+
+        private static AppSettings _appSettings;
+
+        public Uri CreateRequestUri(string baseUri, string relativePath = "")
+        {
+            Uri uri = new Uri(baseUri);
+            Uri endPoint = new Uri(uri, relativePath);
+            UriBuilder uriBuilder = new UriBuilder(endPoint);
+            return uriBuilder.Uri;
+        }
+        public ApiServicesUrls(AppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public string GetBaseUrl<T>()
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}").ToString();
+        }
+
+        public string GetAll<T>()
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}/all").ToString();
+        }
+
+        public string GetAllPaged<T>()
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}/paged").ToString();
+        }
+
+        public string GetDatabable<T>()
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}/datatable").ToString();
+        }
+
+        public string GetByIdUrl<T>()
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}/").ToString();
+        }
+        public string GetByIdUrl<T, TKey>(TKey id)
+        {
+            return CreateRequestUri(ApiBaseUrl, $"{typeof(T).Name}/{id}").ToString();
+        }
+
+
+        public string ApiBaseUrl { get => _appSettings.ApiBaseUrl; }
+    }
+}
