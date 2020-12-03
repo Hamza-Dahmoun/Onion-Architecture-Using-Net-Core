48d5327d0367c723f2221ca05ee15cc5ed09738c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-03 16:28:42 +0100

    add Dossier.cs entity and migrate the changes to the db

---
+++ CompanyName/CompanyName.AppName.Data/Migrations/20201203152637_addDossierModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyName.AppName.Data.Migrations
+{
+    public partial class addDossierModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dossier",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    NumDossier = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dossier", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dossier");
+        }
+    }
+}
---
+++ CompanyName/CompanyName.AppName.Domain/Entities/Dossier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CompanyName.AppName.Domain.Entities
+{
+    public class Dossier
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public Guid id { get; set; }
+
+        [Required(ErrorMessage = "NumDossier is equired")]
+        public string NumDossier { get; set; }
+    }
+}
