cddffe57fd2cefe64e0fbe284498be47a4789583
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-05 00:06:50 +0100

    add api controller ActivityController.cs  register referentielBusinessService inside its base class constructor

---
+++ CompanyName/CompanyName.AppName.API/Controllers/ActivityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyName.AppName.Business;
+using CompanyName.AppName.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.AppName.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : GenericReferentielApiController<Activity, Guid>
+    {
+        public ActivityController(ReferentielBusinessService<Activity, Guid> referentielBusinessService):base(referentielBusinessService)
+        {
+            
+        }
+    }
+}
