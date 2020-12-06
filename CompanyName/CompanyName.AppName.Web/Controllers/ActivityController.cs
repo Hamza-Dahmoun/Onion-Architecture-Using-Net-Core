e7418e2a218545e21751c69b03c79e11ac7e69b4
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-06 14:50:25 +0100

    add ActivityController.cs & register service it needs in its ctor

---
+++ CompanyName/CompanyName.AppName.Web/Controllers/ActivityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyName.AppName.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.AppName.Web.Controllers
+{
+    [Route("[controller/[action]")]
+    public class ActivityController : Controller
+    {
+        private readonly ActivityApiService _activityApiService;
+
+        public ActivityController(ActivityApiService activityApiService)
+        {
+            _activityApiService = activityApiService;
+        }
+    }
+}
