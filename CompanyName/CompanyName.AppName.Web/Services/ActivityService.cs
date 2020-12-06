0d6a7505b42bfeb9ef16ec1182f2a9ad2fa58ebf
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-06 10:32:25 +0100

    add ActivityService.cs class in web project & register/inject services it needs inside its constructor

---
+++ CompanyName/CompanyName.AppName.Web/Services/ActivityService.cs
@@ -0,0 +1,26 @@
+﻿using CompanyName.AppName.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName.Web.Services
+{
+    public class ActivityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AppSettings _appSettings;
+        private readonly ApiServicesUrls _apiServicesUrls;
+        private readonly string _baseUrl;
+
+        public ActivityService(HttpClient httpClient, AppSettings appSettings, ApiServicesUrls apiServicesUrls, string baseUrl)
+        {
+            _httpClient = httpClient;
+            _appSettings = appSettings;
+            _apiServicesUrls = apiServicesUrls;
+            _baseUrl = baseUrl;
+        }
+
+    }
+}
