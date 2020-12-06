bccc0a78cb38879ee7031f6d4ceeb7166a3bc96c
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-06 15:48:04 +0100

    add Index action & its view inside ActivityController.cs

--- CompanyName/CompanyName.AppName.Web/Controllers/ActivityController.cs
+++ CompanyName/CompanyName.AppName.Web/Controllers/ActivityController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyName.AppName.Domain.Entities;
 using CompanyName.AppName.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyName.AppName.Web.Controllers
 {
-    [Route("[controller/[action]")]
+    //[Route("[controller/[action]")]
     public class ActivityController : Controller
     {
         private readonly ActivityApiService _activityApiService;
@@ -15,6 +16,13 @@ namespace CompanyName.AppName.Web.Controllers
         public ActivityController(ActivityApiService activityApiService)
         {
             _activityApiService = activityApiService;
+        }
+
+        public async Task<IActionResult> Index(string term)
+        {
+            List<Activity> model = new List<Activity>();
+
+            return View(model);
         }
     }
 }
