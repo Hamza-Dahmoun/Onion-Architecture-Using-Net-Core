974b5cd9dcbde7cc4d46699cafc080364dec5841
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-07 11:20:11 +0100

    Inside Api/Startup file, register services for AppDbContext & IUnitOfWork, after adding connectionString to API/appsettings.json file

--- CompanyName/CompanyName.AppName.API/Startup.cs
+++ CompanyName/CompanyName.AppName.API/Startup.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CompanyName.AppName.Business;
+using CompanyName.AppName.Data;
 using CompanyName.AppName.Domain.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Reusable.Data.Abstractions;
+using Reusable.Data.Core;
 
 namespace CompanyName.AppName.API
 {
@@ -27,6 +31,13 @@ namespace CompanyName.AppName.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //registering db context services, note that the connection string is the one inside appsettings.json file
+            services.AddScoped<DbContext, AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AppDbConnection")));
+
+            //registering IUnitOfWork services
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             //registering ReferentielBusinessService
             services.AddTransient(typeof(ReferentielBusinessService<,>));
