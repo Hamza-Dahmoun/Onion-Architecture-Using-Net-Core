d724c906c5e392ec48365f5e5842474c9157970f
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-08 15:31:04 +0100

    add Create() get and Create() post methods to ActivityController

--- CompanyName/CompanyName.AppName.Web/Controllers/ActivityController.cs
+++ CompanyName/CompanyName.AppName.Web/Controllers/ActivityController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CompanyName.AppName.Domain.Entities;
+using CompanyName.AppName.Web.Extensions;
 using CompanyName.AppName.Web.Services;
 using Microsoft.AspNetCore.Mvc;
+using Reusable.Business.Core.BusinessResult;
 
 namespace CompanyName.AppName.Web.Controllers
 {
@@ -23,6 +25,40 @@ namespace CompanyName.AppName.Web.Controllers
             List<Activity> model = new List<Activity>();
 
             return View(model);
+        }
+
+
+        [HttpGet]
+        public Task<IActionResult> Create()
+        {
+            return Task.Run(() =>
+            {
+                return View() as IActionResult;
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Activity activite)
+        {
+            BusinessResult businessResult;
+
+            if (ModelState.IsValid)
+            {
+                businessResult = await _activityApiService.PostAsync(activite);
+
+                if (businessResult.Succeeded)
+                {
+                    TempData["Message"] = businessResult.ToBootstrapAlerts();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewData["Message"] = businessResult.ToBootstrapAlerts();
+                }
+            }
+
+            return View(activite);
         }
     }
 }
