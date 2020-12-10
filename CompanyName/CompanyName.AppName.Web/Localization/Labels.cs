73ede983f320f89bd142e6db37711d8e8d91354f
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-10 15:27:21 +0100

    in Web project add classes Labels.cs/Messages.cs/Menu.cs to new 'Localization' folder

---
+++ CompanyName/CompanyName.AppName.Web/Localization/Labels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName
+{
+    public class Labels
+    {
+        //this class is used to be passed to IStringLocalizer<T> when using Localization of different html elements text
+    }
+}
---
+++ CompanyName/CompanyName.AppName.Web/Localization/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName
+{
+    public class Messages
+    {
+        //this class is used to be passed to IStringLocalizer<T> when using Localization of different server messages
+    }
+}
