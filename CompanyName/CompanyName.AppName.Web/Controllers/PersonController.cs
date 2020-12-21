2d3e16edfd582ee76e90dcedc06a778a366d89a1
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2020-12-21 16:16:30 +0100

    add Person/Create action and its view

--- CompanyName/CompanyName.AppName.Web/Controllers/PersonController.cs
+++ CompanyName/CompanyName.AppName.Web/Controllers/PersonController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CompanyName.AppName.Business;
+using CompanyName.AppName.Domain.Entities;
+using CompanyName.AppName.Web.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Reusable.Business.Core.BusinessResult;
 
 namespace CompanyName.AppName.Web.Controllers
 {
@@ -20,6 +23,33 @@ namespace CompanyName.AppName.Web.Controllers
         public IActionResult Index()
         {
             return View(_personBusinessService.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Person person)
+        {
+            BusinessResult businessResult;
+            if (ModelState.IsValid)
+            {
+                businessResult = _personBusinessService.Add(person);
+                if (businessResult.Succeeded)
+                {
+                    TempData["Message"] = businessResult.ToBootstrapAlerts();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewData["Message"] = businessResult.ToBootstrapAlerts();
+                }
+            }
+            return View(person);
         }
     }
 }
